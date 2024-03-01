eb61f8a19b662750d9b0fe6e35366dfc383d2bd6
GoodK-G <141077856+GoodK-G@users.noreply.github.com>
2024-02-29 19:41:53 -0600

    updated the user model!

--- Model/User.js
+++ Model/User.js
@@ -12,45 +12,46 @@ class User extends Model {
 
 User.init(
     {
-        id:{
+        user_id: {
             type: DataTypes.INTEGER,
             allowNull: false,
             primaryKey: true,
-            autoIncrement: true,
+            autoIncrement: true
         },
-        name:{
+        username: {
             type: DataTypes.STRING,
             allowNull: false,
+            unique: true
         },
-        email:{
+        email: {
             type: DataTypes.STRING,
             allowNull: false,
             unique: true,
-            validate:{
-                isEmail: true,
-            },
+            validate: {
+                isEmail: true
+            }
         },
-        password:{
+        password: {
             type: DataTypes.STRING,
             allowNull: false,
-            validate:{
-                len: [8],
-            },
-        },
-        },
-        {
-            hooks:{
-                beforeCreate: async (newUserData) => {
-                    newUserData.password = await bcrypt.hash(newUserData.password, 10);
-                    return newUserData;
-                },
-            },
-            sequelize,
-            timestamps: false,
-            freezeTableName: true,
-            underscored: true,
-            modelName: 'user',
+            validate: {
+                len: [8]
+            }
         }
-    );
+    },
+    {
+        hooks:{
+            beforeCreate: async (newUserData) => {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            }
+        },
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user',
+        sequelize
+    });
 
 module.exports = User;
+  
