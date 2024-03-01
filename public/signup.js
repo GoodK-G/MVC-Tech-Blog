69ef87ffa315fe07f7bdfecb0338b70fc62302d6
GoodK-G <141077856+GoodK-G@users.noreply.github.com>
2024-02-29 19:31:25 -0600

    added signup.js to handle the sign up.

---
+++ public/signup.js
@@ -0,0 +1,21 @@
+
+document.querySelector('.signup-form').addEventListener('submit', signupHandler);
+
+const signupHandler = async (e) => {
+    e.preventDefault();
+    const username = document.querySelector('#username-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+    if (username && password) {
+        const response = await fetch('/api/users/signup', {
+            method: 'POST',
+            body: JSON.stringify({ username, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+        if (response.ok) {
+            alert('You have successfully signed up! Please log in.');
+            document.location.replace('/');
+        } else {
+            alert('Failed to sign up.');
+        }
+    }
+}
